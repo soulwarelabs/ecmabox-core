3aa463b77c0448becbe3c8e0af18bbc3b9f582c5
onlythenaive <ilya.gubarev@gmail.com>
2017-09-21 01:30:04 +0300

    Retention type changed for Discoverable convention

--- source/src/main/java/com/soulwarelabs/ecmabox/convention/Discoverable.java
+++ source/src/main/java/com/soulwarelabs/ecmabox/convention/Discoverable.java
@@ -31,7 +31,7 @@ import java.lang.annotation.Target;
  */
 @Documented
 @Inherited
-@Retention(RetentionPolicy.CLASS)
+@Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.TYPE)
 public @interface Discoverable {
 
