cc57a2fd1fac8f7fa601515788cc1718b08cfe6a
onlythenaive <ilya.gubarev@gmail.com>
2017-09-20 03:38:51 +0300

    Invoice and InvoiceBuilder moved to api.invoice package

--- source/src/main/java/com/soulwarelabs/ecmabox/api/invoice/Invoice.java
+++ source/src/main/java/com/soulwarelabs/ecmabox/api/invoice/Invoice.java
@@ -13,7 +13,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
  */
-package com.soulwarelabs.ecmabox.api;
+package com.soulwarelabs.ecmabox.api.invoice;
 
 import java.util.Objects;
 
--- source/src/main/java/com/soulwarelabs/ecmabox/api/invoice/InvoiceBuilder.java
+++ source/src/main/java/com/soulwarelabs/ecmabox/api/invoice/InvoiceBuilder.java
@@ -13,7 +13,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
  */
-package com.soulwarelabs.ecmabox.api;
+package com.soulwarelabs.ecmabox.api.invoice;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
