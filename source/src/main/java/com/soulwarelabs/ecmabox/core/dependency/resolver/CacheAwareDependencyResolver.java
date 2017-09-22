c19ad0c1983fab282cecf7e53ff684be6b7d0e44
onlythenaive <ilya.gubarev@gmail.com>
2017-09-22 19:01:57 +0300

    Cache-aware dependency resolver added

---
+++ source/src/main/java/com/soulwarelabs/ecmabox/core/dependency/resolver/CacheAwareDependencyResolver.java
@@ -0,0 +1,111 @@
+/*
+   Copyright 2017 Ilia Gubarev
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package com.soulwarelabs.ecmabox.core.dependency.resolver;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import com.soulwarelabs.ecmabox.api.dependency.Dependency;
+import com.soulwarelabs.ecmabox.api.dependency.DependencyOrigin;
+import com.soulwarelabs.ecmabox.api.dependency.DependencyResolver;
+import com.soulwarelabs.ecmabox.api.invoice.Invoice;
+import com.soulwarelabs.ecmabox.api.invoice.InvoiceBuilder;
+import com.soulwarelabs.ecmabox.convention.ImmutableByContract;
+import com.soulwarelabs.ecmabox.convention.Private;
+import com.soulwarelabs.ecmabox.core.cache.Cache;
+import com.soulwarelabs.ecmabox.core.dependency.broker.ResourceScriptBroker;
+
+/**
+ * Cache-based implementation of dependency resolver.
+ *
+ * @see Cache
+ * @see DependencyResolver
+ * @see InvoiceBuilder
+ *
+ * @author Ilia Gubarev
+ */
+@Private
+@ImmutableByContract
+public final class CacheAwareDependencyResolver implements DependencyResolver {
+
+    private final Map<DependencyOrigin, ResourceScriptBroker> brokers;
+    private final Cache<Invoice, Dependency> cache;
+    private final InvoiceBuilder template;
+
+    /**
+     * Creates a new dependency resolver.
+     *
+     * @param brokers script resource brokers.
+     * @param cache resolved dependence cache.
+     * @param template an invoice builder to be used as a template.
+     *
+     * @see Cache
+     * @see InvoiceBuilder
+     * @see ResourceScriptBroker
+     */
+    public CacheAwareDependencyResolver(final Map<DependencyOrigin, ResourceScriptBroker> brokers,
+                                        final Cache<Invoice, Dependency> cache,
+                                        final InvoiceBuilder template) {
+        Objects.requireNonNull(brokers, "Resource script brokers cannot be null");
+        Objects.requireNonNull(cache, "Resource script cache cannot be null");
+        Objects.requireNonNull(template, "Template invoice builder cannot be null");
+        this.brokers = new HashMap<>(brokers);
+        this.cache = cache;
+        this.template = template.copy();
+    }
+
+    @Override
+    public Invoice resolve(final Dependency dependency) {
+        Objects.requireNonNull(dependency, "Dependency cannot be null");
+        return cache
+                .getOrResolve(dependency, this::resolveWithBroker)
+                .orElseThrow(() -> new RuntimeException("Dependency is not resolved"));
+    }
+
+    private Invoice resolveWithBroker(final Dependency dependency) {
+        return template
+                .copy()
+                .description(describe(dependency))
+                .script(receiveScriptFromBroker(dependency))
+                .build();
+    }
+
+    private String receiveScriptFromBroker(final Dependency dependency) {
+        final ResourceScriptBroker broker = broker(dependency);
+        final String result = broker.receive(dependency.getResource());
+        if (result == null) {
+            throw new DependencyResolutionException("Resource script is not received", dependency);
+        }
+        return result;
+    }
+
+    private ResourceScriptBroker broker(final Dependency dependency) {
+        final ResourceScriptBroker result = brokers.get(dependency.getOrigin());
+        if (result == null) {
+            throw new DependencyResolutionException("Suitable broker is not found", dependency);
+        }
+        return result;
+    }
+
+    private String describe(final Dependency dependency) {
+        return String.format(
+                "Dependency invoice for %s %s",
+                dependency.getOrigin(),
+                dependency.getResource()
+        );
+    }
+}
---
+++ source/src/main/java/com/soulwarelabs/ecmabox/core/dependency/resolver/DependencyResolutionException.java
@@ -0,0 +1,83 @@
+/*
+   Copyright 2017 Ilia Gubarev
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package com.soulwarelabs.ecmabox.core.dependency.resolver;
+
+import java.util.Objects;
+
+import com.soulwarelabs.ecmabox.api.dependency.Dependency;
+import com.soulwarelabs.ecmabox.convention.Immutable;
+import com.soulwarelabs.ecmabox.convention.Private;
+
+/**
+ * Dependency resolution exception.
+ *
+ * @see RuntimeException
+ *
+ * @author Ilia Gubarev
+ */
+@Private
+@Immutable
+public final class DependencyResolutionException extends RuntimeException {
+
+    private final Dependency dependency;
+
+    /**
+     * Creates a new exception.
+     *
+     * @param message exception detailed comment.
+     * @param dependency dependency which caused the exception.
+     *
+     * @see Dependency
+     */
+    public DependencyResolutionException(final String message,
+                                         final Dependency dependency) {
+        super(message);
+        requireNotNullDependency(dependency);
+        this.dependency = dependency;
+    }
+
+    /**
+     * Creates a new exception.
+     *
+     * @param message exception detailed comment.
+     * @param dependency dependency which caused the exception.
+     * @param cause the root cause of this exception (optional).
+     *
+     * @see Dependency
+     */
+    public DependencyResolutionException(final String message,
+                                         final Dependency dependency,
+                                         final Throwable cause) {
+        super(message, cause);
+        requireNotNullDependency(dependency);
+        this.dependency = dependency;
+    }
+
+    /**
+     * Gets a dependency which caused the exception.
+     *
+     * @return dependency.
+     *
+     * @see Dependency
+     */
+    public Dependency getDependency() {
+        return dependency;
+    }
+
+    private void requireNotNullDependency(final Dependency dependency) {
+        Objects.requireNonNull(dependency, "Dependency cannot be null");
+    }
+}
