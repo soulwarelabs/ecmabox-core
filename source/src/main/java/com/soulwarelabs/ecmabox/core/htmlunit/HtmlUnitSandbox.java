71f1d2281e59d43b9880dd3b6881438127e0738b
onlythenaive <ilya.gubarev@gmail.com>
2017-09-23 20:40:51 +0300

    HtmlUnit-specific factory implemented

--- source/src/main/java/com/soulwarelabs/ecmabox/core/htmlunit/HtmlUnitSandbox.java
+++ source/src/main/java/com/soulwarelabs/ecmabox/core/htmlunit/HtmlUnitSandbox.java
@@ -15,14 +15,24 @@
  */
 package com.soulwarelabs.ecmabox.core.htmlunit;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+
+import com.gargoylesoftware.htmlunit.ScriptResult;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.javascript.TimeoutError;
 
 import com.soulwarelabs.ecmabox.api.Sandbox;
+import com.soulwarelabs.ecmabox.api.content.Content;
 import com.soulwarelabs.ecmabox.api.inspection.Inspector;
 import com.soulwarelabs.ecmabox.api.invoice.Invoice;
 import com.soulwarelabs.ecmabox.api.log.Record;
 import com.soulwarelabs.ecmabox.api.result.Result;
+import com.soulwarelabs.ecmabox.api.result.ResultTermination;
 import com.soulwarelabs.ecmabox.convention.Private;
+import com.soulwarelabs.ecmabox.core.content.parser.ContentParser;
 
 /**
  * HtmlUnit-specific implementation of sandbox API.
@@ -34,21 +44,69 @@ import com.soulwarelabs.ecmabox.convention.Private;
 @Private
 public final class HtmlUnitSandbox implements Sandbox {
 
+    private final WebClient client;
+    private final HtmlPage page;
+    private final ContentParser parser;
+    private final List<Record> recordsToDrain;
+    private final HtmlUnitRecordStorage recordStorage;
+
+    public HtmlUnitSandbox(final WebClient client,
+                           final HtmlPage page,
+                           final HtmlUnitRecordStorage recordStorage) {
+        Objects.requireNonNull(client, "Sandbox web client cannot be null");
+        Objects.requireNonNull(client, "Sandbox web page cannot be null");
+        Objects.requireNonNull(client, "Sandbox record storage cannot be null");
+        this.client = client;
+        this.page = page;
+        this.parser = HtmlUnitContentParser.getInstance();
+        this.recordsToDrain = new ArrayList<>();
+        this.recordStorage = recordStorage;
+    }
+
     @Override
     public List<Record> drain() {
-        // TODO: implement log records draining
-        throw new UnsupportedOperationException();
+        final List<Record> result = new ArrayList<>(recordsToDrain);
+        recordsToDrain.clear();
+        return result;
     }
 
     @Override
     public Result execute(final Invoice invoice) {
-        // TODO: implement invoice execution
-        throw new UnsupportedOperationException();
+        Objects.requireNonNull(invoice, "Execution invoice cannot be null");
+        final List<Record> currentRecords = new ArrayList<>();
+        try {
+            recordStorage.register(currentRecords::add);
+            setupTimeout(invoice);
+            final ScriptResult scriptResult = this.page.executeJavaScript(invoice.getScript());
+            final Content content = parser.parse(scriptResult.getJavaScriptResult());
+            final ResultTermination termination = ResultTermination.SUCCESS;
+            return new Result(content, invoice, currentRecords, termination);
+        } catch (RuntimeException e) {
+            client.getJavaScriptEngine().shutdown();
+            final ResultTermination termination = terminationType(e);
+            return new Result(Content.undefined(), invoice, currentRecords, termination);
+        }
     }
 
     @Override
     public <T> T inspect(final Inspector<T> inspector) {
-        // TODO: implement sandbox inspection
-        throw new UnsupportedOperationException();
+        Objects.requireNonNull(inspector, "Sandbox inspector cannot be null");
+        return inspector.apply(page, client, recordStorage);
+    }
+
+    private void setupTimeout(final Invoice invoice) {
+        if (invoice.isTimeoutEnabled()) {
+            client.getJavaScriptEngine().setJavaScriptTimeout(invoice.getTimeoutInMilliseconds());
+        } else {
+            client.getJavaScriptEngine().setJavaScriptTimeout(0);
+        }
+    }
+
+    private ResultTermination terminationType(final Throwable throwable) {
+        final Throwable cause = throwable.getCause();
+        if (cause instanceof TimeoutError) {
+            return ResultTermination.TIMEOUT;
+        }
+        return ResultTermination.EXCEPTION;
     }
 }
