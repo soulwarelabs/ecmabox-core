8121cedec90591bf5b693eba13bbbfa6e7b04286
onlythenaive <ilya.gubarev@gmail.com>
2017-09-24 00:41:37 +0300

    SandboxFactory proxy is implemented

--- source/src/main/java/com/soulwarelabs/ecmabox/core/SandboxFactoryProxy.java
+++ source/src/main/java/com/soulwarelabs/ecmabox/core/SandboxFactoryProxy.java
@@ -21,11 +21,24 @@ import java.util.Objects;
 
 import com.soulwarelabs.ecmabox.api.Sandbox;
 import com.soulwarelabs.ecmabox.api.SandboxFactory;
+import com.soulwarelabs.ecmabox.api.dependency.Dependency;
+import com.soulwarelabs.ecmabox.api.dependency.DependencyOrigin;
+import com.soulwarelabs.ecmabox.api.dependency.DependencyResolver;
+import com.soulwarelabs.ecmabox.api.invoice.Invoice;
+import com.soulwarelabs.ecmabox.api.invoice.InvoiceBuilder;
 import com.soulwarelabs.ecmabox.api.layout.EnvironmentType;
 import com.soulwarelabs.ecmabox.api.layout.Layout;
 import com.soulwarelabs.ecmabox.convention.Factory;
 import com.soulwarelabs.ecmabox.convention.Immutable;
 import com.soulwarelabs.ecmabox.convention.Private;
+import com.soulwarelabs.ecmabox.core.cache.Cache;
+import com.soulwarelabs.ecmabox.core.cache.CacheLayout;
+import com.soulwarelabs.ecmabox.core.cache.simple.PerpetualCache;
+import com.soulwarelabs.ecmabox.core.dependency.broker.ResourceScriptBroker;
+import com.soulwarelabs.ecmabox.core.dependency.broker.network.CdnResourceScriptBroker;
+import com.soulwarelabs.ecmabox.core.dependency.broker.network.NetworkLayout;
+import com.soulwarelabs.ecmabox.core.dependency.resolver.CacheAwareDependencyResolver;
+import com.soulwarelabs.ecmabox.core.htmlunit.HtmlUnitSandboxFactory;
 
 /**
  * Proxy-implementation of sandbox factory.
@@ -39,12 +52,7 @@ import com.soulwarelabs.ecmabox.convention.Private;
 @Factory(Sandbox.class)
 public final class SandboxFactoryProxy implements SandboxFactory {
 
-    private final static SandboxFactoryProxy INSTANCE;
-
-    static {
-        final Map<EnvironmentType, SandboxFactory> factories = new HashMap<>();
-        INSTANCE = new SandboxFactoryProxy(factories);
-    }
+    private static SandboxFactoryProxy INSTANCE;
 
     /**
      * Gets an instance of sandbox factory proxy.
@@ -52,7 +60,53 @@ public final class SandboxFactoryProxy implements SandboxFactory {
      * @return sandbox factory proxy instance.
      */
     public static SandboxFactoryProxy getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = createInstance();
+        }
         return INSTANCE;
+    }
+
+    private static SandboxFactoryProxy createInstance() {
+        final Map<EnvironmentType, SandboxFactory> factories = createFactories();
+        return new SandboxFactoryProxy(factories);
+    }
+
+    private static Map<EnvironmentType, SandboxFactory> createFactories() {
+        final Map<EnvironmentType, SandboxFactory> result = new HashMap<>();
+        final SandboxFactory htmlUnitFactory = createHtmlUnitFactory();
+        result.put(EnvironmentType.BROWSER, htmlUnitFactory);
+        return result;
+    }
+
+    private static SandboxFactory createHtmlUnitFactory() {
+        final DependencyResolver defaultResolver = createDependencyResolver();
+        return new HtmlUnitSandboxFactory(defaultResolver);
+    }
+
+    private static DependencyResolver createDependencyResolver() {
+        final Map<DependencyOrigin, ResourceScriptBroker> brokers = createBrokers();
+        final Cache<Invoice, Dependency> cache = createCache();
+        final InvoiceBuilder template = createTemplate();
+        return new CacheAwareDependencyResolver(brokers, cache, template);
+    }
+
+    private static Map<DependencyOrigin, ResourceScriptBroker> createBrokers() {
+        final Map<DependencyOrigin, ResourceScriptBroker> result = new HashMap<>();
+        final NetworkLayout networkLayout = new NetworkLayout();
+        final ResourceScriptBroker cdnBroker = new CdnResourceScriptBroker(null, networkLayout);
+        result.put(DependencyOrigin.CDN, cdnBroker);
+        return result;
+    }
+
+    private static Cache<Invoice, Dependency> createCache() {
+        final CacheLayout cacheLayout = new CacheLayout(true);
+        return new PerpetualCache<>(cacheLayout);
+    }
+
+    private static InvoiceBuilder createTemplate() {
+        return new InvoiceBuilder()
+                .restricted(false)
+                .timeoutEnabled(false);
     }
 
     private final Map<EnvironmentType, SandboxFactory> factories;
