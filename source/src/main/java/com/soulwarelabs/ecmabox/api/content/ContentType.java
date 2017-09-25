78bed9637c4d7228e77410b3d5ee314e4bd6bb1d
onlythenaive <ilya.gubarev@gmail.com>
2017-09-25 17:10:05 +0300

    ContentType improved to check NPE on target Java-type + use specific exception

--- source/src/main/java/com/soulwarelabs/ecmabox/api/content/ContentType.java
+++ source/src/main/java/com/soulwarelabs/ecmabox/api/content/ContentType.java
@@ -19,6 +19,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import com.soulwarelabs.ecmabox.api.content.function.FunctionDescriptor;
 import com.soulwarelabs.ecmabox.convention.Immutable;
@@ -89,7 +90,7 @@ public enum ContentType {
                 return type;
             }
         }
-        throw new IllegalArgumentException("Unknown content value type");
+        throw new ContentParserException(value, "Unknown content value type");
     }
 
     @Nullable
@@ -125,6 +126,7 @@ public enum ContentType {
      * @return <code>true</code> if this type represents the specified Java type.
      */
     public boolean represents(final Class<?> type) {
+        Objects.requireNonNull(type, "Target Java-type cannot be null");
         return javaType != null && javaType.isAssignableFrom(type);
     }
 }
